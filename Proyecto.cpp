e663a35151702a6d5a2503cf04752b2e7f4e5975
=Davidcaxaj <davidcaxaj@umes.edu.gt>
2024-04-29 17:58:27 -0600

    Comit 3

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 //JONATHAN ROGELIO JOCOL MARROQUIN * 202407035 * INGENIERIA EN ELECTRONICA 
 //JOSUÉ DAVID VELÁSQUEZ CAXAJ * 202407031 * INGENIERIA EN ELECTRONICA
-
 string nombre_jugador1,nombre_jugador2; // CADENA DE CARACTERES PARA LOS NOMBRES
 int puntosJugador1=0; // VARIABLES LAS CUALES NOS SERVIRAN PARA GUARDAR EL DATO DE LA SUMA TOTAL DE CADA JUGADOR 
 int puntosJugador2=0;
-int i = 0;  // VARIABLE QUE SERVIRA DENTRO DE LOS CICLOS FOR 
+int i=0;  // VARIABLE QUE SERVIRA DENTRO DE LOS CICLOS FOR 
 bool jugar=true; // VARIABLE BOOLEANA #1
+int opcion;
 
 //FUNCION #1 * AQUI DESIGNAMOS LA CANTIDAD DE FILAS Y OBJETOS QUE DEBE IMPRIMIR EN PANTALLA 
 void menu(int fila1,int fila2,int fila3){
@@ -18,19 +18,20 @@ void menu(int fila1,int fila2,int fila3){
 	cout<<endl;
 	
 	for(i=0;i<fila1; i++){
-		cout<<"&";
+		cout<<"     \3";
 	}
+	cout<<endl;
 	cout<<endl;
 	for(i=0;i<fila2; i++){
-		cout<<"&";
+		cout<<"   \3";
 	}
+	cout<<endl;
 	cout<<endl;
 	for(i=0;i<fila3; i++){
-		cout<<"&";
+		cout<<"  \3";
 	}
 	cout<<endl;
 	cout<<endl;
-	
 }
 	//FUNCION #2, LOOP DEL JUEGO
 	void juego_iniciado(){
@@ -39,15 +40,15 @@ void menu(int fila1,int fila2,int fila3){
 		int fila3=7;
 		int fila=0; // VARIABLE QUE SERVIRA PARA HACER COMPARACIONES CON LAS OTRAS DE FILAS
 		int objetos=0; // VARIABLE QUE SERVIRA PARA HACER RESTAS DE ACUERDO A LA CANTIDAD DE OBJETOS EN CADA FILA
+		bool turno1=true; // VARIABLE BOOLEANA #2
 		
-		bool turnoJugador1=true; // VARIABLE BOOLEANA #2
 		while (true) {
 			
 			//MOSTRAMOS EL MENU 
 			menu(fila1, fila2, fila3);
 			
 			// AQUI DESIGNAMOS TURNOS 
-			if (turnoJugador1) {
+			if (turno1) {
 				cout<<"TURNO DE "<<nombre_jugador1<<":"<<endl;
 			} else {
 				cout<<"TURNO DE "<<nombre_jugador2<<":"<<endl;
@@ -73,18 +74,18 @@ void menu(int fila1,int fila2,int fila3){
 			} else if (fila==3 && objetos<=fila3) {
 				fila3-= objetos;
 			} else {
-				cout << "CANTIDAD INVALIDA!!! PORFAVOR SELECCIONE UNA CANTIDAD EXISTENTE EN LAS FILAS :"<< endl;
+				cout << "CANTIDAD INVALIDA!!! POFAVOR SELECCIONE UNA CANTIDAD EXISTENTE EN LAS FILAS :"<< endl;
 				continue;
 			}
 			// VERIFICAMOS QUIEN RETIRO EL ULTIMO OBJETO
 			if (fila1==0 && fila2==0 && fila3==0) {
-				cout<< nombre_jugador1 <<" Y" << nombre_jugador2 <<" ESTAN EN EMPATE"<<endl;
+				cout<< nombre_jugador1 <<" Y "<< nombre_jugador2 <<" ESTAN EN EMPATE"<<endl;
 				puntosJugador1++;
 				puntosJugador2++;
 				break;
-			}else if(turnoJugador1 || !turnoJugador1){
+			}else if(turno1 || !turno1){
 				if(fila1==0 && fila2==1 && fila3==0){
-					if (turnoJugador1) {
+					if (turno1) {
 						cout<< nombre_jugador1 <<" GANOOO!!!"<< endl;
 						puntosJugador1+=3;
 					} else {
@@ -93,7 +94,7 @@ void menu(int fila1,int fila2,int fila3){
 					}
 					break;
 				}else if(fila1==1 && fila2==0 && fila3==0){
-					if (turnoJugador1) {
+					if (turno1) {
 						cout<< nombre_jugador1 <<" GANOOO !!!"<< endl;
 						puntosJugador1 += 3;
 					}else {
@@ -102,7 +103,7 @@ void menu(int fila1,int fila2,int fila3){
 					}
 					break;
 				}else if(fila1==0 && fila2==0 && fila3==1){
-					if (turnoJugador1) {
+					if (turno1) {
 						cout<< nombre_jugador1 << " GANOOO !!!"<<endl;
 						puntosJugador1+=3;
 					} else {
@@ -113,7 +114,7 @@ void menu(int fila1,int fila2,int fila3){
 				}
 			}
 			// CAMBIAMOS EL TURNO DEL JUGADOR
-			turnoJugador1=!turnoJugador1;
+			turno1=!turno1;
 		}
 		//SE MUESTRAN EL PUNTEO TOTAL DE AMBOS JUGADORES 
 		cout<<"Puntos de " <<nombre_jugador1<<" : "<<puntosJugador1<<endl;
@@ -122,17 +123,35 @@ void menu(int fila1,int fila2,int fila3){
 	}
 		//FUNCION #3
 		bool jugar_nuevamente() {
-			char respuesta;  // VARIABLE QUE NOS SERVIRARA PARA GUARDAR UN CARACTER Y USARLO PARA SEGUIR JUNGANDO O NO 
+			char caracter;  // VARIABLE QUE NOS SERVIRARA PARA GUARDAR UN CARACTER Y USARLO PARA SEGUIR JUNGANDO O NO 
 			do {
 				cout<<"PRESIONE 'a' SI DESEA SEGUIR JUGANDO O 'b' SI DESEA TERMINAR EL JUEGO :("<<endl;
-				cin>>respuesta;
-			} while(respuesta!='a'&& respuesta!='b');
+				cin>>caracter;
+			} while(caracter!='a'&& caracter!='b');
 			
-			return respuesta == 'a';
+			return caracter=='a';
 		}
 		
 		int main() {
-			cout<<"BIENVENIDOS AL JUEGO DE NIM :D"<<endl<<endl<<endl;
+			do{
+				cout<<"ELIJA UNA OPCION"<<endl;
+				cout<<"1. MOSTRAR EL JUEGO DE NIM :)"<<endl;
+				cout<<"2. MOSTRAR FRASES MOTIVADORAS"<<endl;
+				cout<<"3. SALIR"<<endl;
+				cin>>opcion;
+				
+				switch(opcion){
+				case 1: cout<<endl<<endl<<"BIENVENIDOS AL PROGRAMA CREADO POR ESTUDIANTES\nDE PRIMER SEMESTRE DE INGENIERA"<<endl<<endl<<endl;
+				break;
+				case 2: cout<<"SI PUEDES IMAGINARLO PUEDES CREARLO!!!"<<endl;
+				break;
+				default: cout<<"GRACIAS POR TU TIEMPO!!!"<<endl;
+				return 1;
+				}
+			}while(opcion!=1);
+			
+			
+			cout<<endl<<endl<<"BIENVENIDOS AL JUEGO DE NIM :D"<<endl<<endl<<endl;
 			cout << "INGRESE EL NOMBRE DEL JUGADOR #1 :"<<endl;
 			cin >> nombre_jugador1;
 			cout << "INGRESE EL NOMBRE DEL JUGADOR #2 :"<<endl;
@@ -146,4 +165,4 @@ void menu(int fila1,int fila2,int fila3){
 			cout<<"GRACIAS POR JUGAR :)"<<endl;
 			return 0;
 		}
-
+		
